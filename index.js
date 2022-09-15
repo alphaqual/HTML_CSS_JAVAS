d1dc7063b0fcff1ea5abf842db5f7dc84eca66c1
Jit <jit@gmail.com>
2022-09-15 10:29:48 -0500

    updated CSS

---
+++ index.js
@@ -0,0 +1,11 @@
+let country = "India";
+let continent = "Asia";
+let population = "1.3 Billion";
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+//alert(continent)
+//alert(country)
+//alert(population)
